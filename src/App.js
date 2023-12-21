61dd0c0a68637c78c122e8f9a3d876e9875dcd70
Osman728 <osmanisse29@gmail.com>
2023-12-21 16:47:56 +0000

    add structure to VideoDescription component

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.scss";
 import videoDetails from "./data/video-details.json";
 import Header from "./components/Header/Header"
 import Video from "./components/Video/Video"
+import VideoDescription from "./components/VideoDescription/VideoDescription"
 
 function App() {
 const [video, setVideos] = useState(videoDetails);
-  const [selectedVideo, setSelectedVideo] = useState(videoDetails[0]);
+const [selectedVideo, setSelectedVideo] = useState(videoDetails[0]);
   
  
     return (
       <>
         <Header />
         <Video selectedVideo={selectedVideo} />
+        <VideoDescription selectedVideo={selectedVideo} />
       </>
     );
   }
---
+++ src/components/VideoDescription/VideoDescription.js
@@ -0,0 +1,37 @@
+
+import views from '../../assets/images/views.svg';
+import likes from '../../assets/images/likes.svg';
+
+const VideoDescription = ({ selectedVideo }) => {
+  const date = selectedVideo.timestamp;
+  const dateFormat = new Date(date);
+  const formattedMonth = (dateFormat.getMonth() + 1).toString().padStart(2, "0");
+  const formattedDay = dateFormat.getDate().toString().padStart(2, "0");
+  const formattedYear = dateFormat.getFullYear();
+  const formattedDate = `${formattedMonth}/${formattedDay}/${formattedYear}`;
+  return (
+    <div className="videoInfo">
+      <h1 className="videoInfo__title">{selectedVideo.title}</h1>
+      <div className="videoInfo__details">
+        <div className="videoInfo__left-container">
+          <p className="videoInfo__author">{selectedVideo.channel}</p>
+          <p className="videoInfo__date">{formattedDate}</p>
+        </div>
+
+        <div className="videoInfo__right-container">
+          <div className="videoInfo__view-container">
+            <img className="videoInfo__view-icon" src={views} alt="Image views" />
+            <p className="videoInfo__views">{selectedVideo.views}</p>
+          </div>
+
+          <div className="videoInfo__like-container">
+            <img className="videoInfo__like-icon" src={likes} alt="Image likes" />
+            <p className="videoInfo__likes">{selectedVideo.likes}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoDescription;
