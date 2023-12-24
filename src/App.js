c7ea9235bbf56c2bbe6640ca28a58131ad2579f6
Osman728 <osmanisse29@gmail.com>
2023-12-24 15:37:47 +0000

    Add structure and styling to NextVideos component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import { useState } from "react";
 import "./App.scss";
 import videoDetails from "./data/video-details.json";
-import Header from "./components/Header/Header"
-import Video from "./components/Video/Video"
-import VideoDescription from "./components/VideoDescription/VideoDescription"
+import Header from "./components/Header/Header";
+import Video from "./components/Video/Video";
+import VideoDescription from "./components/VideoDescription/VideoDescription";
 import VideoForm from "./components/VideoForm/VideoForm";
 import VideoComments from "./components/VideoComments/VideoComments";
+import videoSideBar from "./data/videos.json";
+import NextVideos from "./components/NextVideos/NextVideos";
+
 function App() {
-const [video, setVideos] = useState(videoDetails);
-const [selectedVideo, setSelectedVideo] = useState(videoDetails[0]);
-  
- 
-    return (
-      <>
-        <Header />
-        <Video selectedVideo={selectedVideo} />
-        <VideoDescription selectedVideo={selectedVideo} />
-        <VideoForm selectedVideo={selectedVideo} />
-        <VideoComments selectedVideo={selectedVideo} />
-      </>
-    );
-  }
-  
-  export default App;
-  
+  const [video, setVideo] = useState(videoDetails);
+  const [selectedVideo, setSelectedVideo] = useState(videoDetails[0]);
+  const [videoList, setVideoList] = useState(videoSideBar);
+
+  const handleFilmClick = (clickedVideo) => {
+    const videoMatch = video.find((videoMatch) => videoMatch.id === clickedVideo.id);
+    if (videoMatch) {
+      setSelectedVideo(videoMatch);
+    }
+  };
+
+  const filteredVideoList = videoList.filter((v) => v.id !== selectedVideo.id);
+  return (
+    <>
+      <Header />
+      <Video selectedVideo={selectedVideo} />
+      <VideoDescription selectedVideo={selectedVideo} />
+      <VideoForm selectedVideo={selectedVideo} />
+      <VideoComments selectedVideo={selectedVideo} />
+      <NextVideos videoSideBar={filteredVideoList} handleFilmClick={handleFilmClick} />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/NextVideos/NextVideos.js
@@ -0,0 +1,20 @@
+import "./NextVideos.scss";
+
+const NextVideos = ({ videoSideBar, handleFilmClick }) => {
+  return (
+    <div>
+      <h1 className="nextvideo__header">Next Video</h1>
+      {videoSideBar.map((video) => (
+        <div key={video.id} className="nextvideo" onClick={() => handleFilmClick(video)}>
+          <img className="nextvideo__image" src={video.image} alt={`${video.title} video thumbnail`} />
+          <div className="nextvideo__info">
+            <p className="nextvideo__info-title">{video.title}</p>
+            <p className="nextvideo__info-author">{video.channel}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default NextVideos;
