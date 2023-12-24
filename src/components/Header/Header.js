3fba5253dbdb6499faa0c924acad357e2427853f
Osman728 <osmanisse29@gmail.com>
2023-12-24 18:54:37 +0000

    add style to header for tablet

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,35 @@
-import brainFlixLogo from "../../assets/images/BrainFlix-logo.svg"
-import profilePicture from "../../assets/images/Mohan-muruge.jpg"
+// Header.jsx
+import React from "react";
+import brainFlixLogo from "../../assets/images/BrainFlix-logo.svg";
+import profilePicture from "../../assets/images/Mohan-muruge.jpg";
 import "./Header.scss";
 
 const Header = () => {
-    return (
-      <header className="header">
-        <img src={brainFlixLogo} className="header__logo" alt="Brain Flix Logo"></img>
+  return (
+    <header className="header">
+      <img src={brainFlixLogo} className="header__logo" alt="Brain Flix Logo" />
+      <div className="header__wrapper-tb">
         <div className="header__container">
-        <input placeholder="Search" className="header__searchbar"></input>
-        <img src={profilePicture} alt="Profile picture"class="header__avartar"/>
+          <input
+            placeholder="Search"
+            className="header__searchbar"
+            type="text"
+          />
+          <img
+            src={profilePicture}
+            alt="Profile picture"
+            className="header__avatar"
+          />
         </div>
         <button className="header__button">Upload</button>
-      </header>
-    );
-  };
-  
-  export default Header;
-  
+        <img
+          src={profilePicture}
+          alt="Profile picture"
+          className="header__avatar-tb"
+        />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
