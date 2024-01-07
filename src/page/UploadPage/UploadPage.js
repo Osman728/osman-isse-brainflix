c2e8f44a4b9d891f4f27eb52b8bc613cec2ecb0d
Osman728 <osmanisse29@gmail.com>
2024-01-07 23:36:09 +0000

    add style and formatthe upload page

--- src/page/UploadPage/UploadPage.js
+++ src/page/UploadPage/UploadPage.js
@@ -1,38 +1,52 @@
 import "./UploadPage.scss";
 import Thumbnail from "../../assets/images/Upload-video-preview.jpg";
-
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 const UploadPage = () => {
+  const navigate = useNavigate();
+  const [buttonText, setButtonText] = useState("PUBLISH");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    setButtonText("PUBLISHED");
+
+    setTimeout(() => {
+      navigate("/");
+    }, 3000);
+  };
   return (
     <section className="upload">
-      <h1 className="upload__title">UploadVideo</h1>
-      <form className="upload__form">
-        <label className="upload__form--label">VIDEO THUMBNAIL</label>
-        <video
-          className="upload__form--thumbnail"
-          poster={Thumbnail}
-          controls
-        ></video>
-        <div className="upload__container">
-          <label className="upload__container--label">TITLE YOUR VIDEO</label>
-          <input
-            className="upload__container--title"
-            name="Title"
-            placeholder="Add a title to your video"
-          ></input>
-          <label  className="upload__container--label">TITLE YOUR VIDEO</label>
-          <textarea
-             className="upload__container--text"
-            name="description"
-            placeholder="Add a description to your video"
-          ></textarea>
+      <h1 className="upload__title">Upload Video</h1>
+      <form className="upload__form" onSubmit={handleSubmit}>
+        <div className="upload__content">
+          <div className="upload__form-thumbnail-container">
+            <label className="upload__form-label">VIDEO THUMBNAIL</label>
+            <video
+              className="upload__form-thumbnail"
+              poster={Thumbnail}
+            ></video>
+          </div>
+          <div className="upload__container">
+            <label className="upload__container-label">TITLE YOUR VIDEO</label>
+            <input
+              className="upload__container-title"
+              name="Title"
+              placeholder="Add a title to your video"
+            ></input>
+            <label className="upload__container-label">TITLE YOUR VIDEO</label>
+            <textarea
+              className="upload__container-text"
+              name="description"
+              placeholder="Add a description to your video"
+            ></textarea>
+          </div>
         </div>
 
         <div className="upload__button">
-        
-         <button  className="upload__button--publish">PUBLISH</button>
-         <button className="upload__button--cancel">CANCEL</button>
+          <button className="upload__button-publish"> {buttonText}</button>
+          <button className="upload__button-cancel">CANCEL</button>
         </div>
-
       </form>
     </section>
   );
