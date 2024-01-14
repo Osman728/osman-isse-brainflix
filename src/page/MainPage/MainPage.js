ed008540d83553e0b693136d89e395dbbf2574ee
Osman728 <osmanisse29@gmail.com>
2024-01-14 17:25:28 +0000

    Implement dynamic video loading and details retrieval

--- src/page/MainPage/MainPage.js
+++ src/page/MainPage/MainPage.js
@@ -8,12 +8,13 @@ import VideoComments from "../../components/VideoComments/VideoComments";
 import NextVideos from "../../components/NextVideos/NextVideos";
 import "./MainPage.scss";
 
-const baseUrl = "https://project-2-api.herokuapp.com/";
-const apiKey = "?api_key=d9e90146-2f9f-428c-bf6f-5bcc5017f253";
-const apiVideos = "videos/";
+
+const apiVideos = "/videos/";
+
+
 
 const MainPage = () => {
-  const { videoId } = useParams();
+  const {videoId } = useParams();
   const [videos, setVideos] = useState(null);
   const [video, setVideo] = useState(null);
   const [isError, setIsError] = useState(false);
@@ -21,7 +22,9 @@ const MainPage = () => {
   useEffect(() => {
     const fetchVideos = async () => {
       try {
-        const response = await axios.get(baseUrl + apiVideos + apiKey);
+        const url = process.env.REACT_APP_API_URL;
+        const response = await axios.get(url + "/videos");
+        console.log(response);
         setVideos(response.data);
       } catch (error) {
         console.error("Error fetching videos:", error);
@@ -31,29 +34,20 @@ const MainPage = () => {
 
     fetchVideos();
   }, []);
-
   useEffect(() => {
     const fetchVideoDetails = async () => {
       try {
-        const response = await axios.get(
-          baseUrl +
-            apiVideos +
-            (videoId || "84e96018-4022-434e-80bf-000ce4cd12b8") +
-            apiKey
-        );
+        const url = process.env.REACT_APP_API_URL;
+        const response = await axios.get(url + apiVideos + (videoId || videos[0].id));
         setVideo(response.data);
       } catch (error) {
         console.log("Error fetching video details:", error);
       }
     };
-
-    if (!videoId) {
-      fetchVideoDetails();
-    } else {
-      fetchVideoDetails();
-    }
-  }, [videoId]);
-
+  
+    fetchVideoDetails();
+  }, [videoId, videos]);
+  
   if (!video) {
     return <main>Loading Selected Video...</main>;
   }
@@ -76,9 +70,7 @@ const MainPage = () => {
           <VideoComments video={video} />
         </div>
         <aside className="related__videos">
-        <NextVideos
-          videos={videos.filter((v) => v.id !== video?.id)}
-        />
+          <NextVideos videos={videos.filter((v) => v.id !== video?.id)} />
         </aside>
       </main>
     </>
@@ -86,3 +78,4 @@ const MainPage = () => {
 };
 
 export default MainPage;
+
