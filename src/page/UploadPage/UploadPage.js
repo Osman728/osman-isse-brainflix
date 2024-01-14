b0f1842346225564645ecc19303b444cec3256a6
Osman728 <osmanisse29@gmail.com>
2024-01-14 23:32:28 +0000

    Implemented form validation and API post functionality

--- src/page/UploadPage/UploadPage.js
+++ src/page/UploadPage/UploadPage.js
@@ -1,20 +1,71 @@
+import React from "react";
 import "./UploadPage.scss";
 import Thumbnail from "../../assets/images/Upload-video-preview.jpg";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+import axios from "axios";
+
 const UploadPage = () => {
   const navigate = useNavigate();
   const [buttonText, setButtonText] = useState("PUBLISH");
+  const [formFields, setFormFields] = useState({
+    title: "",
+    description: "",
+  });
+  const [formErrors, setFormErrors] = useState(false);
 
-  const handleSubmit = (event) => {
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormFields({
+      ...formFields,
+      [name]: value,
+    });
+  };
+
+  const isTitleValid = () => {
+    const isValid = formFields.title.trim() !== "";
+    setFormErrors((prevErrors) => ({
+      ...prevErrors,
+      title: isValid ? "" : "Title is required",
+    }));
+    return isValid;
+  };
+
+  const isDescriptionValid = () => {
+    const isValid = formFields.description.trim() !== "";
+    setFormErrors((prevErrors) => ({
+      ...prevErrors,
+      description: isValid ? "" : "Description is required",
+    }));
+    return isValid;
+  };
+
+  const isFormFieldsValid = () => {
+    return isTitleValid() && isDescriptionValid();
+  };
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
-    setButtonText("PUBLISHED");
+    if (isFormFieldsValid()) {
+      const newVideo = {
+        title: formFields.title,
+        description: formFields.description,
+      };
 
-    setTimeout(() => {
-      navigate("/");
-    }, 3000);
+      await axios.post(process.env.REACT_APP_API_URL + "/videos", newVideo);
+      setButtonText("PUBLISHED");
+
+      setTimeout(() => {
+        navigate("/");
+      }, 3000);
+    }
   };
+
+  const handleCancel = () => {
+    navigate("/");
+  };
+
   return (
     <section className="upload">
       <h1 className="upload__title">Upload Video</h1>
@@ -31,21 +82,39 @@ const UploadPage = () => {
             <label className="upload__container-label">TITLE YOUR VIDEO</label>
             <input
               className="upload__container-title"
-              name="Title"
+              name="title"
               placeholder="Add a title to your video"
+              value={formFields.title}
+              onChange={handleChange}
             ></input>
-            <label className="upload__container-label">TITLE YOUR VIDEO</label>
+            {formErrors.title && (
+              <p className="error-message">{formErrors.title}</p>
+            )}
+            <label className="upload__container-label">
+              ADD A VIDEO DESCRIPTION
+            </label>
             <textarea
               className="upload__container-text"
               name="description"
               placeholder="Add a description to your video"
+              value={formFields.description}
+              onChange={handleChange}
             ></textarea>
+            {formErrors.description && (
+              <p className="error-message">{formErrors.description}</p>
+            )}
           </div>
         </div>
 
         <div className="upload__button">
           <button className="upload__button-publish"> {buttonText}</button>
-          <button className="upload__button-cancel">CANCEL</button>
+          <button
+            className="upload__button-cancel"
+            type="button"
+            onClick={handleCancel}
+          >
+            CANCEL
+          </button>
         </div>
       </form>
     </section>
